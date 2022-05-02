21bdbe44941412e664cd25d463ed5b23961d7866
jaehyeonha <mini0950@knu.ac.kr>
2022-05-02 19:47:22 +0900

    message

---
+++ Week1/j_h_2_0_0_2/1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	vector<int> midget(9);
+	int sum = -100;
+	for (int i = 0; i < 9; i++) {
+		cin >> midget[i];
+		sum += midget[i];
+	}
+
+	for (int i = 0; i < 8; i++) {
+		for (int j = i+1; j < 9; j++) {
+			if (midget[i]+ midget[j] == sum) {
+				midget[i] = -1;
+				midget[j] = -1;
+			}
+		}
+	}
+
+	for (int i = 0; i < 9; i++) {
+		if (midget[i] != -1) {
+			cout << midget[i]<<endl;
+		}
+	}
+	
+}
+
