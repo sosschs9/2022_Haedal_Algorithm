c76076c276fe6ba42d2ecde15fc238540c2faf2e
jaehyeonha <mini0950@knu.ac.kr>
2022-05-16 19:25:03 +0900

    message

--- Week3/j_h_2_0_0_2/1.cpp
+++ Week3/j_h_2_0_0_2/1.cpp
@@ -1,15 +1,17 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
-int main(){
+int main()
+{
 	int n;
-    int count=0;
-	int money[6] = {500,100,50,10,5,1};
-	cin>>n;
-	n=1000-n;
-	for(int i=0;i<6;i++){
-		count+=n/money[i];
-		n%=money[i];	
+	int count = 0;
+	int money[6] = {500, 100, 50, 10, 5, 1};
+	cin >> n;
+	n = 1000 - n;
+	for (int i = 0; i < 6; i++)
+	{
+		count += n / money[i];
+		n %= money[i];
 	}
-	cout<<count<<endl;
+	cout << count << endl;
 }
