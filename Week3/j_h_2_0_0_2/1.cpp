80ad281197755625744146fac77cee501c19df6d
jaehyeonha <100736860+jaehyeonha@users.noreply.github.com>
2022-05-16 14:44:26 +0900

    Create 1.cpp

---
+++ Week3/j_h_2_0_0_2/1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+	int n;
+        int count=0;
+	int money[6] = {500,100,50,10,5,1};
+	cin>>n;
+	n=1000-n;
+	for(int i=0;i<6;i++){
+		count+=n/money[i];
+		n%=money[i];	
+	}
+	cout<<count<<endl;
+}
